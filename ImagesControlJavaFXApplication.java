c1fdb876076c1a17f92ce2a7c76d29efae4dba5a
AgazekiMusayev2009 <128578597+AgazekiMusayev2009@users.noreply.github.com>
2023-12-19 00:09:48 +0400

    Add files via upload

---
+++ ImagesControlJavaFXApplication.java
@@ -0,0 +1,38 @@
+package az.developia.nov30;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ImagesControlJavaFXApplication extends Application{
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage){
+		try {
+			AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("az/developia/nov30/view/main.fxml"));
+		
+			Scene scene = new Scene(pane);
+			
+			stage.setScene(scene);
+			
+			stage.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	
+		
+	}
+
+}
